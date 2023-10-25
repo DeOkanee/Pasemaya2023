5410ac87d5b716c8bc630c2ea66bafa418160ad3
De Oka <115537848+DeOkanee@users.noreply.github.com>
2023-10-25 12:27:23 +0800

    update

--- js/POP UP TUTUP.js
+++ js/POP UP TUTUP.js
@@ -1,49 +1,49 @@
-		/* LOMBA CONDONG*/
-		const condong = document.getElementById("condong");
-		condong.addEventListener("click", function () {
-		  Swal.fire({
-			icon: "info",
-			title: "Oops...",
-			text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
-		  });
-		});
+/* LOMBA CONDONG*/
+const condong = document.getElementById("condong");
+condong.addEventListener("click", function () {
+  Swal.fire({
+    icon: "info",
+    title: "Oops...",
+    text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
+  });
+});
 
-		/* LOMBA GENDER*/
-		const gender = document.getElementById("gender");
-		gender.addEventListener("click", function () {
-		  Swal.fire({
-			icon: "info",
-			title: "Oops...",
-			text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
-		  });
-		});		
+/* LOMBA GENDER*/
+const gender = document.getElementById("gender");
+gender.addEventListener("click", function () {
+  Swal.fire({
+    icon: "info",
+    title: "Oops...",
+    text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
+  });
+});
 
-		/* LOMBA KEKEREB*/
-		const kekereb = document.getElementById("kekereb");
-		kekereb.addEventListener("click", function () {
-		  Swal.fire({
-			icon: "info",
-			title: "Oops...",
-			text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
-		  });
-		});				
+/* LOMBA KEKEREB*/
+const kekereb = document.getElementById("kekereb");
+kekereb.addEventListener("click", function () {
+  Swal.fire({
+    icon: "info",
+    title: "Oops...",
+    text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
+  });
+});
 
-				/* LOMBA OGOH*/
-				const ogoh = document.getElementById("ogoh");
-				ogoh.addEventListener("click", function () {
-				  Swal.fire({
-					icon: "info",
-					title: "Oops...",
-					text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
-				  });
-				});	
+/* LOMBA OGOH*/
+const ogoh = document.getElementById("ogoh");
+ogoh.addEventListener("click", function () {
+  Swal.fire({
+    icon: "info",
+    title: "Oops...",
+    text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
+  });
+});
 
-								/* LOMBA TAPEL*/
-								const tapel = document.getElementById("tapel");
-								tapel.addEventListener("click", function () {
-								  Swal.fire({
-									icon: "info",
-									title: "Oops...",
-									text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
-								  });
-								});	
+/* LOMBA TAPEL*/
+const tapel = document.getElementById("tapel");
+tapel.addEventListener("click", function () {
+  Swal.fire({
+    icon: "info",
+    title: "Oops...",
+    text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
+  });
+});
