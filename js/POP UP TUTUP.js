0cf395d197dc4761661223247f3e86a50e6b4d04
De Oka <115537848+DeOkanee@users.noreply.github.com>
2023-10-25 10:45:22 +0800

    update

---
+++ js/POP UP TUTUP.js
@@ -0,0 +1,27 @@
+const condong = document.getElementById("condong");
+		condong.addEventListener("click", function () {
+		  Swal.fire({
+			icon: "info",
+			title: "Oops...",
+			text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
+		  });
+		});
+
+
+		const gender = document.getElementById("gender");
+		gender.addEventListener("click", function () {
+		  Swal.fire({
+			icon: "info",
+			title: "Oops...",
+			text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
+		  });
+		});		
+
+		const kekereb = document.getElementById("kekereb");
+		kekereb.addEventListener("click", function () {
+		  Swal.fire({
+			icon: "info",
+			title: "Oops...",
+			text: "Pendaftaran dan Ketentuan Sudah Ditutup !",
+		  });
+		});				
